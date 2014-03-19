8e8c7137dcdf7abf2eb3ff58020679b7a98a199c
mkolsen <mkolsen@pluto.dsu.edu>
2014-03-18 23:17:11 -0500

    Ajax added

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,69 @@
+
+class CartsController < ApplicationController
+  before_action :set_cart, only: [:show, :edit, :update, :destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
+
+  def index
+    @carts = Cart.all
+  end
+
+  def show
+  end
+
+  def new
+    @cart = Cart.new
+  end
+  def edit
+  end
+
+  def create
+    @cart = Cart.new(cart_params)
+
+    respond_to do |format|
+      if @cart.save
+        format.html { redirect_to @cart, notice: 'Cart was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @cart }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @cart.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @cart.update(cart_params)
+        format.html { redirect_to @cart, notice: 'Cart was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @cart.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @cart.destroy if @cart.id == session[:cart_id]
+    session[:cart_id] = nil
+    respond_to do |format|
+      format.html { redirect_to store_url }
+      format.json { head :no_content }
+    end
+  end
+
+
+  private
+
+
+    def set_cart
+      @cart = Cart.find(params[:id])
+    end
+
+    def cart_params
+      params[:cart]
+    end
+    def invalid_cart
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      redirect_to store_url, notice: 'Invalid cart'
+    end
+end
